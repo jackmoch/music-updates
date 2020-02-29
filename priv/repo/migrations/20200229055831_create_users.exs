0525cc63424463a00b385a84375e7981e6a59436
Jack Mocherman <mochermanjack@gmail.com>
2020-02-29 00:21:58 -0600

    add timestamps to users table

--- priv/repo/migrations/20200229055831_create_users.exs
+++ priv/repo/migrations/20200229055831_create_users.exs
@@ -6,6 +6,8 @@ defmodule MusicUpdates.Repo.Migrations.CreateUsers do
       add :first_name, :string
       add :last_name, :string
       add :spotify_id, :string
+      
+      timestamps
     end
   end
 end
